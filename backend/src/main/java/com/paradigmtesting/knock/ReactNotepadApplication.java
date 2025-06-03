16a4673a48108cc1b957fa52641bc40dc650663d
Amy Phan <aphan@paradigmtesting.com>
2025-06-03 17:59:39 -0400

    App folder setup

---
+++ backend/src/main/java/com/paradigmtesting/knock/ReactNotepadApplication.java
@@ -0,0 +1,13 @@
+package com.paradigmtesting.knock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactNotepadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactNotepadApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/paradigmtesting/knock/ReactNotepadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paradigmtesting.knock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactNotepadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

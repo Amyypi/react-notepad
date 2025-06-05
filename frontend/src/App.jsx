c1b0ca5838c853d25a2664495e0cbd3f7ca1f2cd
Amy Phan <aphan@paradigmtesting.com>
2025-06-04 22:03:24 -0400

    Fix directory path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from './assets/images/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
     <div>
-        <h1 className="text-3xl font-bold via-blue-400">
+        <h1 className="text-3xl font-bold bg-amber-700">
             Hello worlds
         </h1>
     </div>

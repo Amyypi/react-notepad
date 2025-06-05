786774af1375a329e4ef106284edb389e26b9e86
Amyypi <minhthu_3@yahoo.com>
2025-06-05 01:31:44 -0400

    Fix backend MySQL connection

--- backend/src/test/java/com/paradigmtesting/knock/ReactNotepadApplicationTests.java
+++ backend/src/test/java/com/paradigmtesting/knock/ReactNotepadApplicationTests.java
@@ -2,8 +2,10 @@ package com.paradigmtesting.knock;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ReactNotepadApplicationTests {
 
     @Test
